2dc116e7f5d07925f91e817bfea35ec83dab4256
Harsh Yadav <harshyadav5939@gmail.com>
2025-10-06 00:06:40 +0530

    Initial commit

---
+++ concepts/binarySearch.java
@@ -0,0 +1,37 @@
+// on works on sorted arrays
+// it always return the index of the target element if found
+// otherwise it returns -1
+// time complexity O(log n)
+// space complexity O(1)
+package concepts;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int target = 5;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds
+            // the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
