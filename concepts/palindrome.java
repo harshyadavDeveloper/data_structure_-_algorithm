7a73ef9a33563ac9d94abe0c85634f06d06f084f
harshyadavDeveloper <harshyadav5939@gmail.com>
2026-01-08 16:09:38 +0530

    added some explaination for palindrome

--- concepts/palindrome.java
+++ concepts/palindrome.java
@@ -19,6 +19,27 @@ class Palindrome {
             end--;
         }
         return true;
+
+        // in this solution we are using two pointer approach to check if the string is palindrome or not.
+        // time complexity : O(n)
+        // space complexity : O(1)
+    }
+
+    static boolean isPalindrome2(String str){
+        int n = str.length();
+        char[] reversed = new char[n];
+
+        for(int i = 0; i < n; i++){
+            reversed[i] = str.charAt(n - 1 - i);
+        }
+        String reversedString = new String(reversed);
+
+        return str.equals(reversedString);
+
+        // in this solution we are creating a new reversed string and comparing it with the original string. This approach uses extra space for the reversed string.
+        // time complexity : O(n)
+        // space complexity : O(n) (extra space for reversed string)
+        
     }
 
 }
