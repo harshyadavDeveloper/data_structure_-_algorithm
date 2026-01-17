2e3c86fb04c2259fabee2b8a374070b7e7f4cd5e
harshyadavDeveloper <harshyadav5939@gmail.com>
2026-01-17 11:20:19 +0530

    added optimal solution for remove duplicates from the string

--- practice-problems/3. Remove duplicates from String.java
+++ practice-problems/3. Remove duplicates from String.java
@@ -14,7 +14,16 @@
 
 // brute force algorithm: initialize empty result string. for each character in input : a). loop through result to check if it exists. b). if not found, append. return the result
 
+// time complexity : O(n²) (Nested loops)
+// space complexity : O(n)
+
+// 2. optimal solution: use HashSet to trach the seen character. use stringbuilder to build the string efficiently. traverse the string only once
+
+// optimal solution: create an empty hashset -> create a string builder -> for each character : a) if not seen in the set -> add to the set and also append to the string builder. return the builder as string 
+import java.util.HashSet;
+
 class Solution {
+    // approach 1: using nested loops
 
     public static String removeDuplicates(String s) {
         String result = "";
@@ -37,6 +46,23 @@ class Solution {
         }
 
         return result;
+    }
+
+    // approach 2: using hashset
+    public static String removeDuplicatesWithHashSet(String s){
+        HashSet<Character> seen = new HashSet<>();
+
+        StringBuilder result = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            if(!seen.contains(c)){
+                seen.add(c);
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+
     }
 
 }
