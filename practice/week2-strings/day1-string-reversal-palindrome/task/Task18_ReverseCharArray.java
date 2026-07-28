80b4dc473a7e1372b564a6cef8d78be27eff8f1e
Harsh Yadav <harshyadav5939@gmail.com>
2026-07-28 09:27:18 +0000

    completed and executed task 18

---
+++ practice/week2-strings/day1-string-reversal-palindrome/task/Task18_ReverseCharArray.java
@@ -0,0 +1,22 @@
+// Task 18: Reverse the string "programming" using the char-array two-pointer approach.
+
+class Main{
+    public static void main(String[] args){
+        String original = "programming";
+        char[] array1 = original.toCharArray(); 
+        int start = 0;
+        int end = array1.length - 1;
+
+        while(start<end){
+            char temp = array1[start];
+            array1[start] = array1[end];
+            array1[end] = temp;
+            start++;
+            end--;
+        }
+        String newString = new String(array1);
+        System.out.println(newString);
+
+
+    }
+}
