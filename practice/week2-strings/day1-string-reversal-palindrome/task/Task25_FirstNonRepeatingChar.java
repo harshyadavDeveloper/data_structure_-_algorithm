507e5c84bd190b38d30e8e3b65573ee3a46b079b
Harsh Yadav <harshyadav5939@gmail.com>
2026-07-29 12:17:14 +0000

    solved and executed task 25

---
+++ practice/week2-strings/day1-string-reversal-palindrome/task/Task25_FirstNonRepeatingChar.java
@@ -0,0 +1,34 @@
+// Check if "listen" and "silent" are anagrams (should be true), and separately check "hello" and "world" (should be false).
+
+class Main{
+    public static void main(String[] args){
+
+    }
+
+    boolean isPalindrome(String word1, String word2){
+
+        if(word1.length() != word2.length()){
+            System.out.println("Not a palindrome");
+            return false;
+        }else{
+            int[] freq1 = new int[26];
+            int[] freq2 = new int[26];
+
+            for(int i=0;i<word1.length();i++){
+                freq1[word1.charAt(i) - 'a']++;
+            }
+            for(int i=0;i<word2.length();i++){
+                freq2[word2.charAt(i) - 'a']++;
+            }
+
+            for(int i =0;i<26;i++){
+                if(freq1[i] != freq2[i]){
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+    }
+
+}
