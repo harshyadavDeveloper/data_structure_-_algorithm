38056478b17672c8bde9c00d28b1a24f94869e88
Harsh Yadav <harshyadav5939@gmail.com>
2026-07-29 11:57:42 +0000

    completed and executed task 23

--- practice/week2-strings/day1-string-reversal-palindrome/task/Task23_RemoveDuplicates.java
+++ practice/week2-strings/day1-string-reversal-palindrome/task/Task23_RemoveDuplicates.java
@@ -1,16 +1,16 @@
 // Remove duplicate characters from "programming", keeping only the first occurrence of each letter (expected result: "progamin").
 
-class Main{
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) {
         String word = "programming";
         boolean[] seen = new boolean[26]; // initializes 26 slots with false
         String result = ""; // to store the result
 
-        for(int i=0;i<word.length();i++){
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             int index = c - 'a';
-            if(!seen[index]){
-                result = result+c; // updated the result with the not seen char
+            if (!seen[index]) {
+                result = result + c; // updated the result with the not seen char
                 seen[index] = true; // skip its second and further appreance
             }
         }
