ae5be10fc0d017b541122ea40b5e751877cdd149
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-01 12:08:07 +0000

    added example of merge two sorted array

---
+++ practice/week2-strings/day1-string-reversal-palindrome/task/MergeTwoSortedArray.java
@@ -0,0 +1,40 @@
+// merge {1,3,5} and {2,4,6} into one sorted array {1,2,3,4,5,6}.
+
+class Main{
+    public static void main(String[] args){
+        int[] a = {1,3,5};
+        int[] b = {2,4,6};
+        int[] merged = new int[a.length + b.length];
+
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if(a[i] <= b[j]){
+                merged[k] = a[i];
+                i++;
+
+            }else{
+                merged[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<a.length){
+            merged[k] = a[i];
+            i++;
+            k++;
+        }
+        while(j<b.length){
+            merged[k] = b[j];
+            j++;
+            k++;
+        }
+
+        for(int num: merged){
+            System.out.print(num+" ");
+        }
+
+
+
+    }
+}
