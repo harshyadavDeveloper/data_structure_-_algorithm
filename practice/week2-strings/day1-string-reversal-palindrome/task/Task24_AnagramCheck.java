734fd298425e7af2b6e49e0c5165c87893282e46
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-01 11:44:06 +0000

    added example of two sum

--- practice/week2-strings/day1-string-reversal-palindrome/task/Task24_AnagramCheck.java
+++ practice/week2-strings/day1-string-reversal-palindrome/task/Task24_AnagramCheck.java
@@ -1,7 +1,7 @@
 // Check if "listen" and "silent" are anagrams (should be true), and separately check "hello" and "world" (should be false).
 
-class Main{
-    public static void main(String[] args){
+class Main {
+    public static void main(String[] args) {
         String word1 = "hello";
         String word2 = "world";
 
@@ -14,28 +14,28 @@ class Main{
 
     }
 
-    public static boolean isAnagram(String word1, String word2){
+    public static boolean isAnagram(String word1, String word2) {
 
-        if(word1.length() != word2.length()){
+        if (word1.length() != word2.length()) {
             System.out.println("Not a anagram");
             return false;
-        }else{
+        } else {
             int[] freq1 = new int[26];
             int[] freq2 = new int[26];
 
-            for(int i=0;i<word1.length();i++){
+            for (int i = 0; i < word1.length(); i++) {
                 freq1[word1.charAt(i) - 'a']++;
             }
-            for(int i=0;i<word2.length();i++){
+            for (int i = 0; i < word2.length(); i++) {
                 freq2[word2.charAt(i) - 'a']++;
             }
 
-            for(int i =0;i<26;i++){
-                if(freq1[i] != freq2[i]){
+            for (int i = 0; i < 26; i++) {
+                if (freq1[i] != freq2[i]) {
                     return false;
                 }
             }
-            
+
         }
         return true;
     }
