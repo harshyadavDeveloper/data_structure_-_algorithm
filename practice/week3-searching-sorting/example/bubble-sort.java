590db90e13cb17046e0749bbdef2bc137b0803bb
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-10 11:49:06 +0000

    added and executed bubble-sort

---
+++ practice/week3-searching-sorting/example/bubble-sort.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 8, 1 };
+        int n = arr.length;
+
+        for (int pass = 0; pass < n - 1; pass++) {
+            for (int i = 0; i < n - 1 - pass; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+
+        for(int num: arr){
+            System.out.println(num);
+        }
+    }
+}
