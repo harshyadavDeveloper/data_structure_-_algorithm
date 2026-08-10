6b28ab4d612f995755e02b8ebd78d548598a83d4
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-10 08:50:55 +0000

    solved and executed task 34

---
+++ practice/week3-searching-sorting/day1-linear-binary-search/Task34_LinearSearchCount.java
@@ -0,0 +1,22 @@
+// Task 34: On {4, 12, 7, 19, 3, 25, 8, 16, 1, 30} (unsorted — deliberately, since linear search doesn't care), search for
+//  19 using linear search, and count how many comparisons it took (add a counter that increments each time you check
+//  arr[i] == target).
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { 4, 12, 7, 19, 3, 25, 8, 16, 1, 30 };
+        int target = 19;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            count++;
+            if (arr[i] == target) {
+                System.out.println("Found target at index: " + i);
+                System.out.println("Total counts: " + count);
+                break;
+                
+            }
+        }
+    }
+
+}
