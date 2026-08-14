d9471cbd176f173ab4678b79d637f84f1ea3b8c8
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-14 01:38:29 +0000

    solved and executed task 44

--- practice/week3-searching-sorting/example/insertion-sort.java
+++ practice/week3-searching-sorting/example/insertion-sort.java
@@ -19,7 +19,6 @@ class Main {
 
             arr[j + 1] = key; // found the correct spot — place key there
             count++;
-            
 
         }
         // System.out.println(count);
@@ -30,23 +29,22 @@ class Main {
         int[] arr = { 5, 2, 8, 1 };
         int n = arr.length;
 
-        for(int i=1;i<n;i++){
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
-            int j = i-1;
+            int j = i - 1;
 
-            while(j>=0 && arr[j] > key){
-                arr[j+1] = arr[j];
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
 
         }
-        for(int num:arr){
-            System.out.print(num+ " ");
+        for (int num : arr) {
+            System.out.print(num + " ");
 
         }
         System.out.println();
-        
-        
+
     }
 }
