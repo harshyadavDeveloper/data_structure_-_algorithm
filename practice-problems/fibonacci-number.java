139ee992d61db94f8b4213a94ec2fc069c893da4
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-18 10:50:50 +0530

    added example of fibonacci series

---
+++ practice-problems/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String[] args){
+        int end=10;
+        int first=0;
+        int second=1;
+
+        for(int i=0;i<end;i++){
+            System.out.print(first+", ");
+
+            int next = first+second;
+            first=second;
+            second=next;
+
+        }
+
+    }
+}
