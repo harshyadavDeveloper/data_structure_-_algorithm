41c3ef817da154142d92d6c1661f69ef8c04661f
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-19 14:41:29 +0530

    added peek index function in stack example

--- practice/week4-recursion/day3-4-stacks-queues/example/stack.java
+++ practice/week4-recursion/day3-4-stacks-queues/example/stack.java
@@ -11,27 +11,53 @@ class Main {
         Stack.push(40);
 
         System.out.println("Top: " + Stack.peek());
+        System.out.println("Targeted index: " + Stack.peekIndex(0));
 
     }
 }
 
-class Stack {
-    static int[] stack = new int[10];
-    static int top = -1; // meaning the stack is empty
+// class Stack {
+//     static int[] stack = new int[10]; // this is hardcoded for now
+//     static int top = -1; // meaning the stack is empty
 
-    public static void push(int value) { // adds the value at the very top of the stack
+//     public static void push(int value) { // adds the value at the very top of the stack
+//         top++;
+//         stack[top] = value;
+
+//     }
+
+//     public static int pop() { // removes the last value from the stack
+//         int value = stack[top];
+//         top--;
+//         return value;
+//     }
+
+//     public static int peek() { // lets you see the top value
+//         return stack[top];
+//     }
+// }
+
+class Stack{
+    static int[] stack = new int[10];
+    static int top = -1; // -1 if no values in the stack
+
+    public static void push(int value){
         top++;
         stack[top] = value;
 
     }
 
-    public static int pop() { // removes the last value from the stack
+    public static int pop(){
         int value = stack[top];
         top--;
         return value;
     }
 
-    public static int peek() { // lets you see the top value
+    public static int peek(){
         return stack[top];
+    }
+
+    public static int peekIndex(int value){
+        return stack[value];
     }
 }
