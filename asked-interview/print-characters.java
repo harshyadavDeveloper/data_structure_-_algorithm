c8bb099bf20bca53422b70c81264a7ef74332e13
Harsh Yadav <harshyadav5939@gmail.com>
2026-08-22 17:02:15 +0530

    minor change

--- asked-interview/print-characters.java
+++ asked-interview/print-characters.java
@@ -4,12 +4,12 @@ class Main {
 
     }
 
-    public static void prinChars(){
+    public static void prinChars() {
         String word = "cat";
         int count = 2;
 
-        for(int i=0;i<word.length();i++){
-            for(int j=0;j<count;j++){
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < count; j++) {
                 System.out.print(word.charAt(i) + " ");
             }
             count++;
